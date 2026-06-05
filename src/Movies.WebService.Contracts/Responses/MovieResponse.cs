de6c32d36828554d1552903cdaf150cb51c652e5
felipe d ferreira <felipedferreira11@gmail.com>
2026-06-05 00:08:38 -0400

    chore: remove unused using directive and fix formatting in MovieResponse

--- src/Movies.WebService.Contracts/Responses/MovieResponse.cs
+++ src/Movies.WebService.Contracts/Responses/MovieResponse.cs
@@ -3,7 +3,7 @@
 public class MovieResponse
 {
     public required Guid Id { get; init; }
-    
+
     public required string Title { get; init; } = string.Empty;
 
     public required int YearOfRelease { get; init; }
