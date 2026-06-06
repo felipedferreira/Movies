e0219184b05ec7bfcf0623a61196adfb05d7e5d3
felipe d ferreira <felipedferreira11@gmail.com>
2026-06-06 01:46:38 -0400

    feat: add request and response models for movie operations; update CHANGELOG

---
+++ src/Movies.WebService.Contracts/Requests/UpdateMoviesRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Movies.WebService.Contracts.Requests;
+
+public class UpdateMoviesRequest
+{
+    public required string Title { get; init; } = string.Empty;
+
+    public required int YearOfRelease { get; init; }
+
+    public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+}
